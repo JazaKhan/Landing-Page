b2d6b83183450acd377dddc72674837b022855f5
Jaza Khan <jazakhan294@gmail.com>
2024-08-14 02:13:13 -0700

    updated project list

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,7 +5,6 @@ import project4 from "../assets/projects/project-4.jpg";
 
 export const HERO_CONTENT = `As a UBC Computer Science student and Software Engineering Fellow at Headstarter, I’m driven by a passion for AI and tech innovation. My tech journey started unexpectedly at a bank, where I witnessed firsthand how innovative solutions can streamline operations and enhance customer experiences. This experience ignited my curiosity and set me on a path to develop robust problem-solving and teamwork skills. I am dedicated to creating AI-driven solutions that tackle complex problems and elevate user experiences.`;
 
-
 export const EXPERIENCES = [
   {
     year: "2022 - 2023",
@@ -13,7 +12,6 @@ export const EXPERIENCES = [
     company: "Headstarter",
     description: `Actively participating in a rigorous 7-week software engineering fellowship, where I am honing my technical skills through hands-on projects and intensive learning. Engaging in weekly AI projects, hackathons, and collaborative final projects aimed at real-world impact. This experience is sharpening my abilities in software development, problem-solving, and teamwork, while also providing valuable feedback from industry professionals to accelerate my career trajectory.`,
     technologies: ["HTML", "CSS", "Javascript", "GitHub"],
-    
   },
   {
     year: "2023 - 2024",
@@ -27,15 +25,24 @@ export const EXPERIENCES = [
     role: "Member Service Representative",
     company: "Coast Capital Savings",
     description: `As a Member Service Representative at Coast Capital Savings, I excelled in delivering exceptional customer service by offering personalized financial advice and resolving complex inquiries with precision. My proactive approach in identifying and addressing member needs led to increased satisfaction and strengthened client relationships, driving the branch's performance and growth.`,
-    technologies: ["Team Collaboration", "Communitcation", "Professionalism", "Problem Solving"],
+    technologies: [
+      "Team Collaboration",
+      "Communitcation",
+      "Professionalism",
+      "Problem Solving",
+    ],
   },
   {
     year: "2022 - 2023",
     role: "Sales Associate",
     company: "Pandora",
     description: `As a Sales Associate at Pandora Jewelry, I delivered exceptional customer service by understanding clients' needs and recommending tailored jewelry solutions. I managed inventory efficiently and contributed to achieving sales targets, all while maintaining a high standard of store presentation and creating a memorable shopping experience for each customer.`,
-    technologies: ["Team Collaboration", "Communication", "Customer Service", "Problem Solving"],
-
+    technologies: [
+      "Team Collaboration",
+      "Communication",
+      "Customer Service",
+      "Problem Solving",
+    ],
   },
   {
     year: "2020 - 2022",
@@ -43,7 +50,6 @@ export const EXPERIENCES = [
     company: "McDonald's Canada",
     description: `Delivered exceptional customer service and maintained operational efficiency in a fast-paced environment at McDonald's.`,
     technologies: ["Team Collaboration", "Communication", "Customer Service"],
-
   },
 ];
 
@@ -54,15 +60,15 @@ export const PROJECTS = [
     description:
       "A responsive web-based calculator built with React. It supports basic arithmetic operations and showcases clean design, efficient state management, and an intuitive user interface.",
     technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
-    link: "https://jazakhan.com/"
+    link: "https://jazakhan.com/",
   },
   {
-    title: "Pantry Tracker",
+    title: "PantrySmart",
     image: project2,
     description:
-      "A pantry management app that allows users to track inventory, set expiration reminders, and generate shopping lists. It’s built with Angular and Firebase, focusing on data persistence and real-time updates.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
-    link: "https://jazakhan.com/"
+      "A pantry management app that allows users to track inventory, set and edit quantities, and add additional items.",
+    technologies: ["Next.js", "React", "Firebase"],
+    link: "https://pantry-smart-20svv7hww-jazas-projects-d818107e.vercel.app/",
   },
   {
     title: "AI Customer Support",
@@ -70,7 +76,7 @@ export const PROJECTS = [
     description:
       "An AI-driven chatbot platform designed to automate customer service interactions. It’s built using React and Bootstrap, integrating natural language processing to provide instant responses and streamline customer support operations.",
     technologies: ["HTML", "CSS", "React", "Bootstrap"],
-    link: "https://jazakhan.com/"
+    link: "https://jazakhan.com/",
   },
   {
     title: "AI Flashcards & Stripe",
@@ -78,16 +84,13 @@ export const PROJECTS = [
     description:
       "A flashcard app that utilizes AI to generate personalized learning content. Integrated with Stripe for seamless payments, the platform is built with Vue.js, Express, and mySQL, offering a complete learning and transaction experience.",
     technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
-    link: "https://jazakhan.com/"
+    link: "https://jazakhan.com/",
   },
   {
     title: "Grand Finale",
     image: project3,
-    description:
-      "Undecided for now!",
+    description: "Undecided for now!",
     technologies: ["HTML", "CSS", "React", "Bootstrap"],
-    link: "https://jazakhan.com/"
+    link: "https://jazakhan.com/",
   },
 ];
-
-
